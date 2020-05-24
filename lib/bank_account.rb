8fc2729b9b0541a3f7febe12112d7111d632a12a
Jackson Ramos <jackramos021495@gmail.com>
2020-05-24 21:08:00 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,32 @@
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def self.deposit #method error
+  end
+
+  def display_balance
+    #must have readable balance attribute
+    #show balance
+    #some how make self into balance
+  end
+
+  def valid?
+    #what should it display when its not valid
+    #should show, "open" and balance > 0
+  end
+
+  def close_account
+    #makes status from open to closed
+  end
+
+
+
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,33 @@
 class Transfer
-  # your code here
+  # why is a bank account private?/what makes it private?
+  attr_accessor :sender, :receiver, :status, :transfer_amount
+
+  def initialize(sender, receiver, transfer_amount)
+    #all set/read variables must be initialized
+    @sender = sender
+    @receiver = receiver
+    @transfer_amount = transfer_amount
+    #what makes transfer_amount different than /BankAccount :balance
+    @status = "pending"
+  end
+
+  def valid?
+    #only when TWO accounts are valid
+    #uses BankAccount's valid?
+  end
+
+  def reverse_transfer
+    #reverses execute transaction
+    #must be able to keep track of each transaction
+    #can only be valid transfer
+  end
+
 end
+=begin
+def execute_transaction
+  execution must be successful
+ transfer verifies two accounts
+  subtracts from one account and deposits into another
+  one per Transfer
+  rejects IF sender doesn't have money
+=end
